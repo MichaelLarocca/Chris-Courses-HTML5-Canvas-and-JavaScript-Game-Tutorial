9faa6610c048aee0bcfbdfc955a18d14b1c339dc
Michael J. Larocca <michaellarocca@verizon.net>
2022-11-14 20:07:11 -0500

    Finished end game section

--- index.js
+++ index.js
@@ -102,8 +102,9 @@ function spawnEnemies() {
 	console.log(enemies);
 }
 
+let animationId;
 function animate() {
-	requestAnimationFrame(animate);
+	animationId = requestAnimationFrame(animate);
 	c.clearRect(0, 0, canvas.width, canvas.height);
 	player.draw();
 	projectiles.forEach((projectile) => {
@@ -112,6 +113,13 @@ function animate() {
 
 	enemies.forEach((enemy, index) => {
 		enemy.update();
+
+		const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+		if (dist - enemy.radius - player.radius < 1) {
+			console.log(`End Game!`);
+			cancelAnimationFrame(animationId);
+		}
 
 		projectiles.forEach((projectile, projectileIndex) => {
 			const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
